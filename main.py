74959b951f4b6fdb7fecdb351f4307ff759c8055
renzaki-dev <kbkabulov0225@gmail.com>
2025-09-23 01:24:03 +0500

    Конструкторы, переопределение методов

---
+++ main.py
@@ -0,0 +1,25 @@
+class Cat: 
+  name = None
+  age = None
+  isHappy = None
+
+  def __init__(self, name, age, isHappy = None):
+    self.self_data(name, age, isHappy)
+    self.get_data()
+
+  def self_data(self, name, age, isHappy = None):
+    self.name = name
+    self.age = age
+    self.isHappy = isHappy
+  
+  def get_data(self):
+    print(f"Name: {self.name}")
+    print(f"Age: {self.age}")
+    print(f"Is Happy: {self.isHappy}")
+    print("-----------------------")
+
+
+cat1 = Cat("Murzik", 3, True)
+cat2 = Cat("Barsik", 5, False)
+cat2 = Cat("Barsik", 5)
+
